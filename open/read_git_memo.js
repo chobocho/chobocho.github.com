f1b15c2434104c3065702694349d230c08154d28
chobocho <chobocho99@gmail.com>
2020-10-25 23:34:39 +0900

    [Patch] update link

--- open/read_git_memo.js
+++ open/read_git_memo.js
@@ -1,3 +1,9 @@
 ﻿function init_git_memo_list() {
 	memoList.push(["한컴스페이스", "https://space.malangmalang.com/", "", "한글,말랑말랑,오피스,클라우드,cloud"]);
+	memoList.push(["Tetris", "http://www.chobocho.com/game/tetris/tetris.html", "", "테트리스, block, game"]);
+	memoList.push(["신한생명 모바일 운세", "http://m.shinhanlife.co.kr/svc/haezon.jsp", "", "사주"]);
+	memoList.push(["신한생명 운세", "http://www.shinhanlife.co.kr/bigLife.do", "", "사주"]);
+	memoList.push(["디벨로퍼웍스 코리아 dW Korea - IBM Developer", "https://developer.ibm.com/kr/", "", "ibm, 개발"]);
+	memoList.push(["모던 JavaScript 튜토리얼", "https://ko.javascript.info/", "", "개발, javascript, 자바스크립트"]);
+	memoList.push(["[유머] 제갈량", "https://m.clien.net/service/board/park/15129599?type=recommend", "", "유머, 제갈량, 클리앙"]);
 }
