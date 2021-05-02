30268c42498ba37d8469ca7fb1d101d303a267e5
chobocho <chobocho99@gmail.com>
2021-05-02 11:45:06 +0900

    [Patch] update link

--- open/read_git_memo.js
+++ open/read_git_memo.js
@@ -1,24 +1,31 @@
 ﻿function init_git_memo_list() {
-	memoList.push(["한컴스페이스", "https://space.malangmalang.com/", "", "한글,말랑말랑,오피스,클라우드,cloud"]);
-	memoList.push(["Tetris", "http://www.chobocho.com/game/tetris/tetris.html", "", "테트리스, block, game"]);
-	memoList.push(["신한생명 모바일 운세", "http://m.shinhanlife.co.kr/svc/haezon.jsp", "", "사주"]);
-	memoList.push(["신한생명 운세", "http://www.shinhanlife.co.kr/bigLife.do", "", "사주"]);
-	memoList.push(["디벨로퍼웍스 코리아 dW Korea - IBM Developer", "https://developer.ibm.com/kr/", "", "ibm, 개발"]);
-	memoList.push(["모던 JavaScript 튜토리얼", "https://ko.javascript.info/", "", "개발, javascript, 자바스크립트"]);
-	memoList.push(["[유머] 제갈량", "https://m.clien.net/service/board/park/15129599?type=recommend", "", "유머, 제갈량, 클리앙"]);
- 	memoList.push(["부산 개발자 커뮤니티", "https://cafe.naver.com/itcommuinty", "", "카페, cafe"]);
-	memoList.push(["GeeksForGeeks", "https://www.geeksforgeeks.org/", "", "자료구조, 알고리즘, Data structure, Algorithms"]);
-	memoList.push(["HP Museum", "https://www.hpmuseum.org/", "", "HP IT 박물관"]);
-	memoList.push(["UnderKG", "http://underkg.co.kr/", "", "IT, Mobile, 모바일 소식"]);
-	memoList.push(["코딩 도장", "https://dojang.io/", "", "it, study, python, cpp"]);
-	memoList.push(["Repl", "https://repl.it/~", "", "code, study, python, cpp, codig"]);
-	memoList.push(["Naver game", "https://game.naver.com", "", "game,naver"]);
-	memoList.push(["Trend Talk", "https://news.trendtalk.kr", "", "china, trend, it, mobile, wechat"]);
-	memoList.push(["부산대 학생 커뮤니티", "https://mypnu.net/", "", "communite,게시판,pnu,busan"]);
-	memoList.push(["맥심코리아", "https://www.maximkorea.net/", "", "maxim, 19, 잡지"]);
-	memoList.push(["월간 HR Insight", "https://www.hrinsight.co.kr/", "", "HR, 인사, 잡지"]);
-	memoList.push(["https://editor.p5js.org/", "p5js", "", "javascript,자바스크립트,온라인,에디터"]);
-	memoList.push(["Leetcode", "https://leetcode.com/", "", "code,coding,test,certi"]);
-	memoList.push(["프로그래머스", "https://programmers.co.kr/", "", "코딩, 써티, study"]);
+    memoList.push(["한컴스페이스", "https://space.malangmalang.com/", "", "한글,말랑말랑,오피스,클라우드,cloud"]);
+    memoList.push(["Tetris", "http://www.chobocho.com/game/tetris/tetris.html", "", "테트리스, block, game"]);
+    memoList.push(["신한생명 모바일 운세", "http://m.shinhanlife.co.kr/svc/haezon.jsp", "", "사주"]);
+    memoList.push(["신한생명 운세", "http://www.shinhanlife.co.kr/bigLife.do", "", "사주"]);
+    memoList.push(["디벨로퍼웍스 코리아 dW Korea - IBM Developer", "https://developer.ibm.com/kr/", "", "ibm, 개발"]);
+    memoList.push(["모던 JavaScript 튜토리얼", "https://ko.javascript.info/", "", "개발, javascript, 자바스크립트"]);
+    memoList.push(["[유머] 제갈량", "https://m.clien.net/service/board/park/15129599?type=recommend", "", "유머, 제갈량, 클리앙"]);
+    memoList.push(["부산 개발자 커뮤니티", "https://cafe.naver.com/itcommuinty", "", "카페, cafe"]);
+    memoList.push(["GeeksForGeeks", "https://www.geeksforgeeks.org/", "", "자료구조, 알고리즘, Data structure, Algorithms"]);
+    memoList.push(["HP Museum", "https://www.hpmuseum.org/", "", "HP IT 박물관"]);
+    memoList.push(["UnderKG", "http://underkg.co.kr/", "", "IT, Mobile, 모바일 소식"]);
+    memoList.push(["코딩 도장", "https://dojang.io/", "", "it, study, python, cpp"]);
+    memoList.push(["Repl", "https://repl.it/~", "", "code, study, python, cpp, codig"]);
+    memoList.push(["Naver game", "https://game.naver.com", "", "game,naver"]);
+    memoList.push(["Trend Talk", "https://news.trendtalk.kr", "", "china, trend, it, mobile, wechat"]);
+    memoList.push(["부산대 학생 커뮤니티", "https://mypnu.net/", "", "communite,게시판,pnu,busan"]);
+    memoList.push(["맥심코리아", "https://www.maximkorea.net/", "", "maxim, 19, 잡지"]);
+    memoList.push(["월간 HR Insight", "https://www.hrinsight.co.kr/", "", "HR, 인사, 잡지"]);
+    memoList.push(["https://editor.p5js.org/", "p5js", "", "javascript,자바스크립트,온라인,에디터"]);
+    memoList.push(["Leetcode", "https://leetcode.com/", "", "code,coding,test,certi"]);
+    memoList.push(["프로그래머스", "https://programmers.co.kr/", "", "코딩, 써티, study"]);
+    memoList.push(["Okky", "https://okky.kr/articles/community"]);
+    memoList.push(["BBC Korean", "http://www.bbc.com/korean", "", "plain_style"]);
+    memoList.push(["보배드림", "https://www.bobaedream.co.kr/"]);
+    memoList.push(["오늘의 유머 베스트 게시판", "http://m.todayhumor.co.kr/list.php?table=bestofbest"]);
+    memoList.push(["오늘의 유머", "http://m.todayhumor.co.kr/"]);
+    memoList.push(["82cook", "https://www.82cook.com/entiz/enti.php?bn=15"]);
+  
 }
 
